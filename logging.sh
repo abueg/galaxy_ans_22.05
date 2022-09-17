ad0931d7a4903748e8bc310ad4148ce401dbdeae
vgl_galaxy <vgl_galaxy@vglgalaxy.hpc.rockefeller.internal>
2022-09-16 22:53:27 -0400

    saaaxax

--- logging.sh
+++ logging.sh
@@ -2,8 +2,8 @@
 
 export NOW=$(date +"%Y%b%d_%H%M%S")
 export LOGPATH=$SCRATCH/galaxy_srv/galaxy/var/gravity/log
-mv $SCRATCH/galaxy_srv/galaxy/var/gravity/log/gunicorn.log $SCRATCH/galaxy_log_backups/gunicorn_${NOW}.log
-mv $SCRATCH/galaxy_srv/galaxy/var/gravity/log/handler_0.log $SCRATCH/galaxy_log_backups/handler_0_${NOW}.log
-mv $SCRATCH/galaxy_srv/galaxy/var/gravity/log/handler_1.log $SCRATCH/galaxy_log_backups/handler_1_${NOW}.log
-mv $SCRATCH/galaxy_srv/galaxy/var/gravity/log/handler_2.log $SCRATCH/galaxy_log_backups/handler_2_${NOW}.log
+mv $LOGPATH/gunicorn.log $SCRATCH/galaxy_log_backups/gunicorn_${NOW}.log
+mv $LOGPATH/handler_0.log $SCRATCH/galaxy_log_backups/handler_0_${NOW}.log
+mv $LOGPATH/handler_1.log $SCRATCH/galaxy_log_backups/handler_1_${NOW}.log
+mv $LOGPATH/handler_2.log $SCRATCH/galaxy_log_backups/handler_2_${NOW}.log
 echo Log back up done at `date`.
