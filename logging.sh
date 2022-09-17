2e047ef9178b6963f23102f6d569e7b7e3ff7963
vgl_galaxy <vgl_galaxy@vglgalaxy.hpc.rockefeller.internal>
2022-09-16 23:14:52 -0400

    frasdfafa

--- logging.sh
+++ logging.sh
@@ -6,4 +6,6 @@ mv $LOGPATH/gunicorn.log $SCRATCH/galaxy_log_backups/gunicorn_${NOW}.log
 mv $LOGPATH/handler_0.log $SCRATCH/galaxy_log_backups/handler_0_${NOW}.log
 mv $LOGPATH/handler_1.log $SCRATCH/galaxy_log_backups/handler_1_${NOW}.log
 mv $LOGPATH/handler_2.log $SCRATCH/galaxy_log_backups/handler_2_${NOW}.log
+mv $LOGPATH/handler_3.log $SCRATCH/galaxy_log_backups/handler_3_${NOW}.log
+mv $LOGPATH/handler_4.log $SCRATCH/galaxy_log_backups/handler_4_${NOW}.log
 echo Log back up done at `date`.
