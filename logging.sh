49fe308019c58cf2694a8d726251c36060c928ae
vgl_galaxy <vgl_galaxy@vglgalaxy.hpc.rockefeller.internal>
2022-09-22 13:22:05 -0400

    change bionano max mem

--- logging.sh
+++ logging.sh
@@ -6,5 +6,4 @@ mv $LOGPATH/gunicorn.log $SCRATCH/galaxy_log_backups/gunicorn_${NOW}.log
 mv $LOGPATH/handler_0.log $SCRATCH/galaxy_log_backups/handler_0_${NOW}.log
 mv $LOGPATH/handler_1.log $SCRATCH/galaxy_log_backups/handler_1_${NOW}.log
 mv $LOGPATH/handler_2.log $SCRATCH/galaxy_log_backups/handler_2_${NOW}.log
-mv $LOGPATH/handler_3.log $SCRATCH/galaxy_log_backups/handler_3_${NOW}.log
 echo Log back up done at `date`.
