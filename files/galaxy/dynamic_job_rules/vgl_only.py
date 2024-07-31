8e7b237c093354cb90d77212ae0ca3bd9ab0ef21
abueg <linelle.abueg@gmail.com>
2024-07-31 14:56:18 -0400

    24.1 migration

--- files/galaxy/dynamic_job_rules/vgl_only.py
+++ files/galaxy/dynamic_job_rules/vgl_only.py
@@ -1,7 +1,7 @@
 from galaxy.jobs import JobDestination
 from galaxy.jobs.mapper import JobMappingException
 import os
-VGL_EMAILS = ["admin@admin.org","njain@rockefeller.edu","labueg@rockefeller.edu","cjohnson02@rockefeller.edu","ofedrigo@rockefeller.edu","ptraore@rockefeller.edu","sburmeiste@rockefeller.edu"]
+VGL_EMAILS = ["admin@admin.org","njain@rockefeller.edu","labueg@rockefeller.edu","cjohnson02@rockefeller.edu","ofedrigo@rockefeller.edu","ptraore@rockefeller.edu","sburmeiste@rockefeller.edu","gformenti@rockefeller.edu"]
 
 def vgl_only(user_email):
     if user_email in VGL_EMAILS:
